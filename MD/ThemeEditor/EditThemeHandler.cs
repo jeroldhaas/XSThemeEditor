c6ae91716886f6f749e02a3cd118a0b2f31772a4
jeroldhaas <jeroldhaas@gmail.com>
2013-11-30 23:20:51 -0500

    Added initial menu handler.

---
+++ MD/ThemeEditor/EditThemeHandler.cs
@@ -0,0 +1,22 @@
+using System;
+using MonoDevelop.Components.Commands;
+
+namespace XSThemeEditor
+{
+	class EditThemeHandler : CommandHandler
+	{
+		protected override void Run ()
+		{
+		}
+		
+		protected override void Update(CommandInfo info)
+		{
+		}
+	}
+	
+	public enum EditTheme
+	{
+		EditTheme,
+	}
+}
+
