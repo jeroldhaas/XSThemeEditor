342503ba653afd26f79d148efb9d703b51d15a93
Jerold Haas <jeroldhaas@gmail.com>
2013-12-23 11:07:17 -0500

    initial set-up craziness

---
+++ XSThemeEditor/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+module XSThemeEditor.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+
+[<assembly: AssemblyTitle("XSThemeEditor")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Jerold Haas")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
